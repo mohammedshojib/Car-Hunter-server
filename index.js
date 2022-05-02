61a1c50e4ea63fdcaebb1f744550ea6c54bfe0cb
mohammedshojib <asmdshojib11@gmail.com>
2022-05-02 16:52:11 +0600

    error and apis added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,25 @@ const app = express();
 //<===== Cors ====>
 app.use(cors());
 app.use(express.json());
+
+//<===== Verify Auth ====>
+
+function verifyAuth(req, res, next) {
+  const auth = req.headers.authorization;
+  if (!auth) {
+    return res.status(401).send({ message: "unauthorize" });
+  }
+  const token = auth.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "details not verified" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+//<===== START HERE ====>
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n3upn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -21,16 +41,60 @@ async function run() {
   try {
     await client.connect();
     const carCollection = client.db("car").collection("carCollection");
+    const myCarsCollection = client.db("car").collection("myCars");
+
+    //<===== AUTH ====>
+
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send(token);
+    });
 
     //<===== ALL CARS ====>
 
     app.get("/cars", async (req, res) => {
       const query = {};
       const cursor = carCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
+      const cars = await cursor.toArray();
+      res.send(cars);
     });
+
+    //<===== MY ALL CARS ====>
+
+    app.get("/my-items", verifyAuth, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email == decodedEmail) {
+        const query = { email: email };
+        const cursor = myCarsCollection.find(query);
+        const myItems = await cursor.toArray();
+        res.send(myItems);
+      } else {
+        res
+          .status(403)
+          .send({ message: "Boo! Your details are fake Login again" });
+      }
+    });
+
+    //<===== MY CARS UPLOAD ====>
+
+    app.post("/mycars", async (req, res) => {
+      const myCars = req.body;
+      if (!myCars.name || !myCars.img) {
+        return res.send({
+          succsess: false,
+          error: "Plase provide all information",
+        });
+      }
+      const result = await myCarsCollection.insertOne(myCars);
+      res.send(result);
+    });
+
     //<===== UPDATE CARS ====>
+
     app.put("/car/:id", async (req, res) => {
       const id = req.params.id;
 
@@ -60,11 +124,20 @@ async function run() {
       res.send(result);
     });
 
-    // <====DELETE CAR===>
+    // <==== DELETE CAR===>
+
     app.delete("/car/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await carCollection.deleteOne(query);
+      res.send(result);
+    });
+    // <==== DELETE MY CAR===>
+
+    app.delete("/my-items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await myCarsCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
